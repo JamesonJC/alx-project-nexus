7921e7283169528c5c0f5d2c6e44811a92273bfb
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-09-27 11:24:34 +0000

    Fix: On setting.py additions for databases connetions in settings.py missing character

--- jobboard_backend/core/settings.py
+++ jobboard_backend/core/settings.py
@@ -115,8 +115,7 @@ DATABASES = {
         default=config('DATABASE_URL'),
         conn_max_age=600,
         engine='django.db.backends.postgresql'
-    ),
-    
+    )    
         #postgresql://username:password@host:port/dbname,
         # "ENGINE": "django.db.backends.sqlite3",
         # "NAME": BASE_DIR / "db.sqlite3",
